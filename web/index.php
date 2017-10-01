6855bd61d643155104b978ace1870c825f8d7826
Vital Krasilnikau <vital.krasilnikau@gmail.com>
2017-10-01 22:46:33 +0300

    Make php

--- web/index.php
+++ web/index.php
@@ -1,25 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Form test</title>
+</head>
+<body>
 
-require('../vendor/autoload.php');
+  <?php if (isset($_GET['submitted'])) { ?>
+    <h1>The server had accepted:</h1>
+    <p>radio: <?php echo htmlspecialchars($_GET['radio']); ?></p>
+    <p>Try again? <a href="/form">Yes!</a></p>
+  <?php } ?>
 
-$app = new Silex\Application();
-$app['debug'] = true;
+  <?php if (!isset($_GET['submitted'])) { ?>
+  
+  <h1>Play with the HTML form</h1>
+  <form action="/form">
+    <input type="hidden" name="submitted" value="yes">
+    <p>
+      <label for="my-radio-yes">radio Yes: </label>
+      <input id="my-radio-yes" name="radio" type="radio" required value="Yes">
+      <label for="my-radio-no">radio No: </label>
+      <input id="my-radio-no" name="radio" type="radio" required value="No"></p>
+    <br>
+    <input type="submit" value="Submit">
+    <input type="reset" value="Clear">
+  </form>
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+  <?php } ?>
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+</body>
+</html>
